f5a4e9efe1745fe7a60af244228afcc3f7e3256b
Harshika Govind <harshika0102@gmail.com>
2023-05-04 15:41:25 +0530

    added code for fetch api branch and created a new branch

---
+++ src/components/quote.js
@@ -0,0 +1,64 @@
+import '../App.css';
+import React, { useState } from 'react';
+
+function Quote() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const category = 'success';
+
+  const getQuotes = async () => {
+    const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+    try {
+      setLoading(true);
+      fetch(url, {
+        method: 'GET',
+        withCredentials: true,
+        headers: {
+          'X-Api-Key': '4zb2YJzIg2E3oaKCJJP6xA==LwAtU3SF0SIbvgeq',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setQuote(data[0].quote);
+          setAuthor(data[0].author);
+        });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      // print error to console for debugging purposes
+    }
+  };
+
+  useState(() => {
+    getQuotes();
+  }, []);
+
+  const handleClick = () => {
+    getQuotes();
+  };
+
+  return (
+    <div className="quote-container">
+      <h2>
+        {quote}
+        <span className="author">
+          &nbsp;(
+          { author}
+          )
+        </span>
+      </h2>
+      <button
+        type="button"
+        onClick={handleClick}
+        className="quote-button"
+      >
+        {loading ? 'Loading..' : 'New Quote'}
+      </button>
+    </div>
+  );
+}
+
+export default Quote;
