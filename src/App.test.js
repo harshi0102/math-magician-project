6b4e45c8b326b6fc4044c730bc0a22b4960f9ee8
Harshika Govind <harshika0102@gmail.com>
2023-05-10 18:01:46 +0530

    Created new branch where added code for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Math Magician link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Math Magician/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -6,19 +6,20 @@ const Home = () => (
     <h2>Welcome to our page</h2>
 
     <p>
-      Calculators used to perform mathematical calculations. It has become an essential tool
-      in  everyday life for students, professionals, and anyone
-      who needs to perform calculations quickly and accurately.
-      Calculators can perform a range of functions, from simple arithmetic operations like
-      addition, subtraction, multiplication, and division to complex mathematical equations
-      and scientific calculations.
+      Mathematics, the science of structure, order, and relation that has evolved from elemental
+      practices of counting, measuring, and describing the shapes of objects. It deals with
+      logical reasoning and quantitative calculation, and its development has involved an
+      increasing degree of idealization and abstraction of its subject matter. Since the 17th
+      century, mathematics has been an indispensable adjunct to the physical sciences and
+      technology, and in more recent times it has assumed a similar role in the quantitative
+      aspects of the life sciences.
     </p>
     <p>
-      The basic calculator has a numeric keypad and a display screen, while more
-      advanced calculators may have additional features like graphing capabilities,
-      programmability, and scientific functions.
-      With the advent of smartphones, calculators have also been integrated into
-      mobile devices,making them more accessible than ever before.
+      In many cultures—under the stimulus of the needs of practical pursuits, such as
+      commerce and agriculture—mathematics has developed far beyond basic counting. This
+      growth has been greatest in societies complex enough to sustain these activities and
+      to provide leisure for contemplation and the opportunity to build on the achievements
+      of earlier mathematicians.
     </p>
 
   </div>
