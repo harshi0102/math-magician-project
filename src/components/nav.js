2df3ba6516a84d098c4a58ace27cc5931a54ed3a
Harshika Govind <harshika0102@gmail.com>
2023-05-12 12:43:13 +0530

    added changes in the nav for deploying

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,31 @@
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <nav className="navBar">
     <h1>Math Magician</h1>
     <ul className="menuNav">
-      <li><a href="/">Home</a></li>
+      {/* <li><a href="/">Home</a></li>
       <li><a href="/calculator">Calculator</a></li>
-      <li><a href="/quotes">Quotes</a></li>
+      <li><a href="/quotes">Quotes</a></li> */}
+
+      <Link to="/home">
+        <li className="menuNav" title="home">
+          Home
+        </li>
+      </Link>
+
+      <Link to="/calculator">
+        <li className="menuNav" title="calculator">
+          Calculator
+        </li>
+      </Link>
+
+      <Link to="/quotes">
+        <li className="menunav" title="quotes">
+          Quotes
+        </li>
+      </Link>
     </ul>
   </nav>
 );
